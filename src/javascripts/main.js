aa0412734ffed27b84e52eebb5e223de1430ddfa
Joseph Cyrus <Joseph.Cyrus@valtech.com>
2020-07-22 17:31:09 +0100

    Initial set up
    Install NPM, Vue, WebPack

---
+++ src/javascripts/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from '../App.vue';
+import './modules/songs';
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+});
---
+++ src/javascripts/modules/songs.js
@@ -0,0 +1,5 @@
+const getAllSongs = () => {
+    console.log('HELLO');
+};
+
+getAllSongs();
